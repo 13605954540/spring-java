08e60f2bab3499b2668d8c48620e5303e9afe289
13605954540 <LinLin1314520>
2023-02-06 07:39:49 +0800

    Nacos maven与配置

--- springcloud/spring-cloud-provider/src/main/java/org/example/controller/ProviderTestController.java
+++ springcloud/spring-cloud-provider/src/main/java/org/example/controller/ProviderTestController.java
@@ -1,19 +1,19 @@
 package org.example.controller;
 
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RefreshScope
-@CrossOrigin
 @RestController
 @RequestMapping("/provider-test")
-public class ConsumerTestController {
+public class ProviderTestController {
+
+    @Value("${server.port}")
+    private String port;
 
     @GetMapping("/go")
-    private String update() {
-        return "provider-test";
+    private String test() {
+        return "port: " + port;
     }
 }
--- springcloud/spring-cloud-server/src/main/java/org/example/controller/ServerTestController.java
+++ springcloud/spring-cloud-server/src/main/java/org/example/controller/ServerTestController.java
@@ -1,23 +1,32 @@
-package org.example;
+package org.example.controller;
 
 import org.example.bean.Man;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/server-test")
 public class ServerTestController {
 
     @Autowired
+    public RestTemplate restTemplate;
+
+    @Autowired
     private Man man;
 
     @GetMapping("/go")
-    private String test() {
-        return "name: " + man.getName() + "   age: " + man.getAge();
+    private ResponseEntity<String> test() {
+        for(int i = 0; i < 20; i++) {
+            ResponseEntity<String> res = restTemplate.getForEntity("http://spring-cloud-provider/provider-test/go", String.class);
+            System.err.println(res.getBody());
+        }
+        return null;
     }
 }
