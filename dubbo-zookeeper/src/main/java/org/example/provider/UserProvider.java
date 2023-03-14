3f824526a66594550e97ec13baf86adc1a7520ba
598622595@qq.com <598622595@qq.com>
2023-03-14 23:10:26 +0800

    dubbo接口提交

--- dubbo-zookeeper/src/main/java/org/example/provider/UserProvider.java
+++ dubbo-zookeeper/src/main/java/org/example/provider/UserProvider.java
@@ -5,4 +5,10 @@ import org.example.bean.User;
 public interface UserProvider {
 
     User getUser(User user);
+
+    User getUser(Long id, String name, Integer age);
+
+    User getUser2(Long id, String name, Integer age);
+
+    User getUser3();
 }
--- dubbo-zookeeper/src/main/java/org/example/provider/impl/UserProviderImpl.java
+++ dubbo-zookeeper/src/main/java/org/example/provider/impl/UserProviderImpl.java
@@ -1,6 +1,7 @@
 package org.example.provider.impl;
 
 import org.apache.dubbo.config.annotation.Service;
+import org.apache.dubbo.metadata.definition.model.FullServiceDefinition;
 import org.example.bean.User;
 import org.example.provider.UserProvider;
 
@@ -13,5 +14,21 @@ public class UserProviderImpl implements UserProvider {
         user.setAge(user.getAge() + 1);
         user.setId(user.getId() + 1);
         return user;
+    }
+
+    @Override
+    public User getUser(Long id, String name, Integer age) {
+        System.err.println("------------------------ dubbo被请求到了哦 ------------------------");
+        return new User().setId(id).setName(name).setAge(age);
+    }
+
+    @Override
+    public User getUser2(Long id, String name, Integer age) {
+        return new User().setId(id).setName(name).setAge(age);
+    }
+
+    @Override
+    public User getUser3() {
+        return new User().setId(1L).setName("1L").setAge(1);
     }
 }
